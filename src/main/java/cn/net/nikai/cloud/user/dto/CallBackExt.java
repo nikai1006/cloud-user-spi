cd2c43364f0826fb57f4e455c84f4760a912bc91
nikai <nikai1006@126.com>
2023-03-20 10:15:29 +0800

    add test dto

---
+++ src/main/java/cn/net/nikai/cloud/user/dto/CallBackExt.java
@@ -0,0 +1,14 @@
+package cn.net.nikai.cloud.user.dto;
+
+import org.and.api.base.spi.ServiceApi;
+
+public class CallBackExt extends ServiceApi {
+
+    /**
+     * 描述信息
+     * @default 无
+     * @example 调度回调接口
+     */
+    private String desc;
+
+}
